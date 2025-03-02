b450db359ec0e5f1217b8df4d120af1d7487408e
ImMrHak <hakkoumohamed23@gmail.com>
2025-03-02 14:17:05 +0000

    Init ToDoList Manager Project

---
+++ src/main/java/com/tdlm/TdlmApplication.java
@@ -0,0 +1,11 @@
+package com.tdlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TdlmApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tdlm/TdlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
